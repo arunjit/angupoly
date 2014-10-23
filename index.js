1aed44d39ad5488b7efdad927289e029edb10911
Arunjit Singh <opensrc@ajsd.in>
2014-10-23 18:41:53 +0530

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+angular.module('angupoly', []).
+run(function(){
+  document.getElementById('menubtn').addEventListener('click', function() {
+    document.getElementById('drawerpnl').togglePanel();
+  }, false);
+}).
+controller('Controller', function($log) {
+  this.selected = 0;
+  this.searchText = '';
+  this.searching = false;
+
+  this.next = function() {
+    this.selected = (this.selected + 1) % 3;
+    $log.info(this.selected);
+  };
+
+  this.search = function() {
+    $log.info('search()');
+    if (this.searchText && this.searching) {
+      $log.info('Searching...');
+      return;  // actually do the search
+    }
+    this.searching = true;
+  };
+
+  this.cancelSearch = function() {
+    this.searching = false;
+    this.searchText = '';
+  };
+}).
+directive('apSelected', function() {
+  return function(scope, element, attrs) {
+    attrs.$observe('apSelected', function(value) {
+      element[0].selected = value;
+    });
+  }
+}).
+directive('apEnter', makeKeyDownDirective('apEnter', 13)).
+directive('apEsc', makeKeyDownDirective('apEsc', 27)).
+directive('apFocuswhen', function() {
+  return function(scope, element, attrs) {
+    attrs.$observe('apFocuswhen', function(value) {
+      value && element[0].focus();
+    });
+  };
+});
+
+function makeKeyDownDirective(name, keyCode) {
+  return function(/*$parse*/) {
+    return function(scope, element, attrs) {
+      element.bind('keydown', function(event) {
+        (event.keyCode == keyCode) && scope.$apply(attrs[name]);
+      });
+    };
+  };
+}
